98d6b68b0a77b076e4edb72c1a8147f439980644
Alexander <iguminov05@yandex.ru>
2022-10-27 20:59:55 +0300

    second_1 commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 import '../scss/style.scss';
+import '../scss/nullstyle.scss';
+import '../scss/header.scss';
+import '../scss/title.scss';
+import '../scss/slideMenu.scss';
+import '../scss/content.scss';
 
 console.log('Works!');
